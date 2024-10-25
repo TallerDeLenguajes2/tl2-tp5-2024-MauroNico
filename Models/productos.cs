14342423908ef7eb742ad258816a2dadde009f74
MauroNico <su>
2024-10-25 19:17:52 -0300

    models agregados

---
+++ Models/productos.cs
@@ -0,0 +1,11 @@
+namespace Tienda.Models{
+    public class Producto{
+        private int idProducto;
+        private string descripcion;
+        private int precio;
+
+        public int IdProducto { get => idProducto; set => idProducto = value; }
+        public string Descripcion { get => descripcion; set => descripcion = value; }
+        public int Precio { get => precio; set => precio = value; }
+    }
+}
